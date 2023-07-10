ba2f8dff2d979358695d858e3c84bfbf1aaec556
Betsabe Amaguai <betsabe.amaguai@epn.edu.ec>
2023-07-09 20:53:10 -0500

    implementar iInicializar recyclerView y aumentarTotalLikes

--- 01-Android/app/src/main/java/com/example/movilessoftware2023a/MainActivity.kt
+++ 01-Android/app/src/main/java/com/example/movilessoftware2023a/MainActivity.kt
@@ -54,6 +54,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val botonCicloVida = findViewById<Button>(
             R.id.btn_ciclo_vida
         )
